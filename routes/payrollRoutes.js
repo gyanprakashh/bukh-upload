bb4e1e632ecabf46451d9a62f81544a8487e9114
Gyan Prakash <38151144+gyanprakashh@users.noreply.github.com>
2025-01-19 23:22:52 +0530

    Update payrollRoutes.js

--- routes/payrollRoutes.js
+++ routes/payrollRoutes.js
@@ -2,6 +2,6 @@ const express = require("express");
 const router = express.Router();
 const { generatePayroll } = require("../controllers/payrollController");
 
-router.post("/", generatePayroll);
+//router.post("/", generatePayroll);
 
 module.exports = router;
