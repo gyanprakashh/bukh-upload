37f084820316ef9930760cfb077dfda8f73a4ba3
Gyan Prakash <38151144+gyanprakashh@users.noreply.github.com>
2025-01-19 22:36:23 +0530

    Update app.js

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ app.use(bodyParser.json());
 
 // Routes 
 //app.use("/api/employees", employeeRoutes);
-app.use("/api/payroll", payrollRoutes);
+//app.use("/api/payroll", payrollRoutes);
 app.get("/", (req, res)=>{
     res.send("api is running")
 })
